ff1b5b4da48627ec1ae75f839f2612aad86a1ff2
Michel Succar <m.succar@cern.ch>
2026-05-08 16:27:08 +0200

    Resolve timezone diparity for session block

--- indico/util/marshmallow.py
+++ indico/util/marshmallow.py
@@ -479,7 +479,7 @@ class EventTimezoneDateTimeField(fields.DateTime):
     def _serialize(self, value, attr, obj, **kwargs):
         if value is None:
             return None
-        new_value = value.astimezone(self.context['event'].tzinfo).replace(tzinfo=None)
+        new_value = value.astimezone(self.context['event'].tzinfo)
         return super()._serialize(new_value, attr, obj, **kwargs)
 
     def _deserialize(self, value, attr, data, **kwargs):
